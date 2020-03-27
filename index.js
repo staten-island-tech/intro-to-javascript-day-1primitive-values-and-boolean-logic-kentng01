4b59d2b7642d93c5f370e175142e5947ddb43b32
kentng01 <57498837+kentng01@users.noreply.github.com>
2020-03-27 10:34:56 -0400

    pogs

---
+++ index.js
@@ -0,0 +1,77 @@
+/*let string = "string";
+const edis = "edis";
+let sentence = "Hello " + "Vicky";
+let sentence2 = " and also Angel";
+//let combine = sentence + sentence2;
+let combine = `${sentence}${sentence2}`; //ES2015
+
+let student1 = "Sandra";
+let student2 = "Kelly";
+let courseName = "AP CSP";
+let teachername = "Whalen";
+
+let year = 2020;
+let nextYear = 2020 + 1;*/
+
+/*let trueStatement = true;
+let falseStatement = false;
+
+let undefinedVar; */
+//console.Log(underfinedVar); //do this a lot
+//console.log(nonExistent); //null never declared
+//console.log(truuStatement);
+
+//if/ else if statements
+//let student = "Karen";
+//let age = 17;
+//console.log(student === "Karen");//true
+//console.log(student === "karen");//false
+//console.log(student == "karen");//false
+//console.log(age === "17"); //false
+//console.log(age == "17"); //true
+
+//let age = 16;
+//let name = "Kristy";
+/*console.log(age < 17);
+console.log(age <= 17);
+console.log(age =< 17);*/
+//console.log(age == 16 | | age == 17); //if age equals 16 OR age == 17 then true
+//console.log(age == 16 && name === "Angel");
+
+let light = light;
+if (light === "green"){
+    console.log("go");
+}
+else if(light === "yellow"){
+    console.log("caution");
+}
+else (light === "red"){
+    console.log("stop");
+}
+
+let kristyLacI = false;
+let baoLacI = false;
+
+if(kristyLacI === true && baoLacI == true){
+    console.log("almond milk")
+}
+else if (kristyLacI === true || baoLacI == true){
+    console.log("both");
+}
+else{
+    console.log("milk");
+}
+
+let temp = 67;
+if(temp < 68){
+    console.log("cold");
+}
+else if (temp >= 68 && temp <= 72){
+    console.log("perfect");
+}
+else if (temp > 72 && temp <= 76){
+    console.log("warm");
+}
+else{
+    console.log("hot");
+}
